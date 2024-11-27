df01070847b4032273a4154809cef914752dd194
kimyou1102 <yoosk5485@naver.com>
2024-11-27 18:22:42 +0900

    feat: 로또 상금에 대한 객체 상수화

---
+++ src/constant/lotto.js
@@ -0,0 +1,7 @@
+export const lottoPrize = {
+  1: { matchCount: 6, prize: 2_000_000_000 },
+  2: { matchCount: 5, prize: 30_000_000 },
+  3: { matchCount: 5, prize: 1_500_000 },
+  4: { matchCount: 4, prize: 50000 },
+  5: { matchCount: 3, prize: 5000 },
+};
