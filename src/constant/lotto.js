4ccdc5c761bf554a3e0392724fc473257508d4ca
kimyou1102 <yoosk5485@naver.com>
2024-11-29 19:22:02 +0900

    refactor: 상수 객체의 안전한 사용을 위한 깊은 동결 적용

--- src/constant/lotto.js
+++ src/constant/lotto.js
@@ -1,15 +1,17 @@
-export const lottoPrize = {
+import { deepFreeze } from '../utils/deepFreeze.js';
+
+export const lottoPrize = deepFreeze({
   1: { matchCount: 6, prize: 2_000_000_000 },
   2: { matchCount: 5, prize: 30_000_000 },
   3: { matchCount: 5, prize: 1_500_000 },
   4: { matchCount: 4, prize: 50000 },
   5: { matchCount: 3, prize: 5000 },
-};
+});
 
-export const lottoRank = {
+export const lottoRank = deepFreeze({
   1: 0,
   2: 0,
   3: 0,
   4: 0,
   5: 0,
-};
+});
--- src/constant/message.js
+++ src/constant/message.js
@@ -1,6 +1,8 @@
+import { deepFreeze } from '../utils/deepFreeze.js';
+
 export const ERROR_PREFIX = '[ERROR]';
 
-export const ERROR_MESSAGES = {
+export const ERROR_MESSAGES = deepFreeze({
   PURCHASE_PRICE: {
     NOT_A_NUMBER: '구입금액은 숫자로만 입력 가능합니다.',
     INVALID_UNIT: '구입금액은 1,000원 단위로 입력해야 합니다.',
@@ -25,11 +27,11 @@ export const ERROR_MESSAGES = {
   },
   INPUT_ERROR: '입력 중 오류가 발생했습니다.',
   EMPTY_INPUT: '입력값으로 빈값을 입력할 수 없습니다.',
-};
+});
 
-export const INPUT_MESSAGE = {
+export const INPUT_MESSAGE = deepFreeze({
   PURCHASE_PRICE: '\n구입금액을 입력해 주세요.',
   WINNING_NUMBER: '\n당첨 번호를 입력해 주세요.',
   BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.',
   RESULT: '\n당첨 통계\n---',
-};
+});
